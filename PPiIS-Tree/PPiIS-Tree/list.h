18512ec41d658a2936640449720823a44504a47a
Антон Клинцов <92170048@study.bsuir.by>
2020-12-24 21:41:24 +0300

    set_list

--- PPiIS-Tree/PPiIS-Tree/list.h
+++ PPiIS-Tree/PPiIS-Tree/list.h
@@ -10,6 +10,7 @@ public:
         _list(nullptr)
     {    }
     void push_back(const T& data);
+    void del(const T& data);
 private:
     unique_ptr<T[]> _list;
     int _size;
@@ -25,4 +26,24 @@ inline void list<T>::push_back(const T& data)
     ndata[_size] = data;
     _list = ndata;
     ++size;
+}
+
+template<class T>
+inline void list<T>::del(const T& data)
+{
+    unique_ptr<T[]> deldata = unique_ptr<T[]>(new T[size - 1]);
+
+    int i = 0;
+    while (_list[i] != data)
+    {
+        deldata[i] = _list[i];
+        ++i;
+    }
+    while (i < _size - 1)
+    {
+        deldata[i] = _list[i + 1];
+        ++i;
+    }
+    _list = deldata;
+    --_size;
 }
